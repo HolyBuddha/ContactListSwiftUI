316cd6bcd2792a7b242f31c4011ca62cf0b49f5a
Vladimir Izmaylov <holybuddha@yandex.ru>
2021-11-13 12:00:48 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Vladimir Izmaylov on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
